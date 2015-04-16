5beda729bd2252d704ff02c02356494ddcd39aaa
daizihang <zander.dai@gmail.com>
2015-04-16 14:43:45 -0400

    print c

--- src/SGD/adagrad.cpp
+++ src/SGD/adagrad.cpp
@@ -22,23 +22,22 @@ adagrad::~adagrad () {
 
 void adagrad::updateParams (float *params, float *grad, int rank) {
 	m_stepCount += 1;
-	if (m_stepCount > 500) {
-		for (int i=0; i<m_nParamSize; i++) {
-			m_histSquareGrad[i] += grad[i] * grad[i];
-			params[i] -= m_learningRate * grad[i] / sqrt(m_histSquareGrad[i]);
-		}
-	} else {
-		float sum = 0.f;
-		for (int i=0; i<m_nParamSize; i++) {
-			m_histSquareGrad[i] += grad[i] * grad[i];
-			sum += sqrt(m_histSquareGrad[i]);
-		}
-			
-		float rate = m_learningRate * sum / sqrt(sqrt(m_stepCount));
-		printf("step[%d]: sum %f, rate %f, m_learningRate %f\n", m_stepCount, sum, rate, m_learningRate);
+	
+	// for (int i=0; i<m_nParamSize; i++) {
+	// 	m_histSquareGrad[i] += grad[i] * grad[i];
+	// 	params[i] -= m_learningRate * grad[i] / sqrt(m_histSquareGrad[i]);
+	// }
+	
+	float sum = 0.f;
+	for (int i=0; i<m_nParamSize; i++) {
+		m_histSquareGrad[i] += grad[i] * grad[i];
+		sum += sqrt(m_histSquareGrad[i]);
+	}
 		
-		for (int i=0; i<m_nParamSize; i++) {
-			params[i] -= rate * grad[i] / sqrt(m_histSquareGrad[i]);
-		}
+	float rate = m_learningRate * sum / sqrt(sqrt(m_stepCount));
+	printf("step[%d]: sum %f, rate %f, m_learningRate %f\n", m_stepCount, sum, rate, m_learningRate);
+	
+	for (int i=0; i<m_nParamSize; i++) {
+		params[i] -= rate * grad[i] / sqrt(m_histSquareGrad[i]);
 	}
 }
