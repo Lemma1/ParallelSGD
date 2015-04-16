fa739d4dd91742b9a21c4417e06fdba09e8fbb04
daizihang <zander.dai@gmail.com>
2015-04-16 14:04:37 -0400

    print c

--- src/SGD/adagrad.cpp
+++ src/SGD/adagrad.cpp
@@ -31,10 +31,11 @@ void adagrad::updateParams (float *params, float *grad, int rank) {
 		m_histSquareGrad[i] += grad[i] * grad[i];
 		sum += sqrt(m_histSquareGrad[i]);
 	}
-	m_stepCount += 1;
-	printf("step[%d]: %f\n", m_stepCount, sum);
+	m_stepCount += 1;	
+	float rate = m_learningRate * sum / sqrt(sqrt(m_stepCount));
+	printf("step[%d]: sum %f, rate %d\n", m_stepCount, sum, rate);
 	
 	for (int i=0; i<m_nParamSize; i++) {
-		params[i] -= m_learningRate * grad[i] * sum / (sqrt(m_histSquareGrad[i]) * sqrt(sqrt(m_stepCount)));
+		params[i] -= rate * grad[i] * sum / sqrt(m_histSquareGrad[i]);
 	}
 }
